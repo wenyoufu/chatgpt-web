eaffba3db1bb9722c5943bb6119988f741e9d1bc
ford <1334788676@qq.com>
2023-04-26 20:18:09 +0800

    提交个人代码

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -49,15 +49,15 @@ onMounted(() => {
           此项目开源于
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
+            href="https://github.com/wenyoufu/chatgpt-web"
             target="_blank"
           >
             GitHub
           </a>
-          ，免费且基于 MIT 协议，没有任何形式的付费行为！
+          ，由fordwen搭建，源于github开源代码，如需溯源，请查看fork源，
         </p>
         <p>
-          如果你觉得此项目对你有帮助，请在 GitHub 帮我点个 Star 或者给予一点赞助，谢谢！
+          希望大家多多支持开源项目，谢谢！
         </p>
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://github.com/wenyoufu/testaaaaaa/blob/master/mingren.jpg',
+      name: 'Fordwen',
+      description: 'Star on <a href="https://github.com/wenyoufu/chatgpt-web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
